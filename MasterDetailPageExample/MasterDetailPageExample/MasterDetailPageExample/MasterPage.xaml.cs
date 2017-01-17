b03814f7b73088e4cef111e6cb32c2ff3c79719d
Juan Ramon <juan.santiago2@hotmail.com>
2017-01-16 20:58:35 -0600

    Proyecto de Xamarin Forms en Visual studio donde se realiza un ejemplo con MasterDetailPage

---
+++ MasterDetailPageExample/MasterDetailPageExample/MasterDetailPageExample/MasterPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MasterDetailPageExample
+{
+    public partial class MasterPage : ContentPage
+    {
+        public ListView ListView { get { return listView; } }
+        public MasterPage()
+        {
+            InitializeComponent();
+
+            var masterPageItem = new List<MasterPageItem>();
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "HomePage",
+                IconSource = "Icon.png",
+                TargetType = typeof(HomePage)
+            });
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "CSSPage",
+                IconSource = "ic_action_css.png",
+                TargetType = typeof(CSSPage)
+            });
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "JavascriptPage",
+                IconSource = "ic_action_javascript.png",
+                TargetType = typeof(JavascriptPage)
+            });
+            masterPageItem.Add(new MasterPageItem
+            {
+                Title = "HtmlPage",
+                IconSource = "ic_action_html.png",
+                TargetType = typeof(HtmlPage)
+            });
+
+            listView.ItemsSource = masterPageItem;
+        }
+    }
+}
